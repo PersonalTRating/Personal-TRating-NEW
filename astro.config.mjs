1267af528afe0563bb6e485156bb7a0a1ebae91b
PersonalTRating <modernisetraders@hotmail.com>
2026-08-05 21:11:38 +0100

    Replace sitemap plugin with static sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://personaltrating.com',
-  integrations: [
-    tailwind(),
-    sitemap({
-      filter: (page) => !page.includes('/trainers/') && !page.includes('/review/'),
-    }),
-  ],
+  integrations: [tailwind()],
   output: 'static',
 });
